6bd25d5a200897c642ee555e222d41941524c53e
Sam Page <samnpage96@gmail.com>
2023-10-25 15:48:23 -0400

    Setup connection to DbContextOptions

---
+++ ElevenNote.Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ElevenNote.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+}
