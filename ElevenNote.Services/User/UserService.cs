2950002b6a26b31a168eb9de8ebd8052d7e27318
Sam Page <samnpage96@gmail.com>
2023-10-25 21:40:38 -0400

    GetUsername and GetEmail Helper methods added

--- ElevenNote.Services/User/UserService.cs
+++ ElevenNote.Services/User/UserService.cs
@@ -1,10 +1,11 @@
 using ElevenNote.Data;
 using ElevenNote.Data.Entities;
 using ElevenNote.Models.User;
+using Microsoft.EntityFrameworkCore;
 
 namespace ElevenNote.Services.User;
 
-public class UserService : IUserService 
+public class UserService : IUserService
 {
     // Field
     private readonly ApplicationDbContext _context;
@@ -16,6 +17,10 @@ public class UserService : IUserService
     }
     public async Task<bool> RegisterUserAsync(UserRegister model)
     {
+        //  checks the returned value from both methods. If either return anything but null, we'll know it's invalid data.
+        if (await GetUserByEmailAsync(model.Email) != null || await GetUserByUsernameAsync(model.UserName) != null)
+            return false;
+
         // Calls our UserEntity entity and applys each property value collected to its respective property.
         UserEntity entity = new()
         {
@@ -25,6 +30,7 @@ public class UserService : IUserService
             DateCreated = DateTime.Now
         };
 
+        //Checks if username exists in the database or not.      
         // Adds our new entity object to _context.Users DbSet. This will add the entity to the Users table.
         _context.Users.Add(entity);
         // Returns number of rows changed in the db and stores it into a variable.
@@ -32,5 +38,19 @@ public class UserService : IUserService
 
         // returns a boolean value of true because we are expecting at least a single change.
         return numberOfChanges == 1;
+
+    }
+
+    // Helper Methods
+    // Checks whether the user's email is unique
+    private async Task<UserEntity?> GetUserByEmailAsync(string email)
+    {
+        return await _context.Users.FirstOrDefaultAsync(user => user.Email.ToLower() == email.ToLower());
+    }
+
+    // Checks whether the user's username is unique
+    private async Task<UserEntity?> GetUserByUsernameAsync(string username)
+    {
+        return await _context.Users.FirstOrDefaultAsync(user => user.Username.ToLower() == username.ToLower());
     }
 }
