1a24ed686b9986e6184fe84d1438db5993d826b5
Sam Page <samnpage96@gmail.com>
2023-10-26 19:58:38 -0400

    comment added to dbcontext constructor

--- ElevenNote.Data/ApplicationDbContext.cs
+++ ElevenNote.Data/ApplicationDbContext.cs
@@ -5,6 +5,7 @@ namespace ElevenNote.Data;
 
 public class ApplicationDbContext : DbContext
 {
+    // Constructor that passes in options that apply to dbcontext that pulls from program.cs. (passing it the connection string.)
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
     public DbSet<UserEntity> Users { get; set; } = null!;
