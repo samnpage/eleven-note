81df07d2593e6bfc19284ad4ee1eb07f140355b5
Sam Page <samnpage96@gmail.com>
2023-10-27 11:52:46 -0400

    Get endpoint added

--- ElevenNote.WebApi/Controllers/UserController.cs
+++ ElevenNote.WebApi/Controllers/UserController.cs
@@ -36,4 +36,17 @@ public class UserController : ControllerBase
 
         return BadRequest(new TextResponse("User could not be registered."));
     }
+
+    [HttpGet("{userId:int}")]
+    public async Task<IActionResult> GetById([FromRoute] int userId)
+    {
+        UserDetail? detail = await _userService.GetUserByIdAsync(userId);
+
+        if (detail is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(detail);
+    }
 }
