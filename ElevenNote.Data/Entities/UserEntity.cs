f9e75feb15d1714a52e0b3d6480aa67d70da08f2
Sam Page <samnpage96@gmail.com>
2023-10-26 20:34:59 -0400

    IdentityUser inheritence added. Removed unnecessary properties

--- ElevenNote.Data/Entities/UserEntity.cs
+++ ElevenNote.Data/Entities/UserEntity.cs
@@ -1,21 +1,10 @@
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
 
 namespace ElevenNote.Data.Entities;
 
-public class UserEntity
+public class UserEntity : IdentityUser<int>
 {
-    [Key]
-    public int Id { get; set; }
-
-    [Required, EmailAddress]
-    public string Email { get; set; } = string.Empty;
-
-    [Required]
-    public string Username { get; set; } = string.Empty;
-
-    [Required]
-    public string Password { get; set; } = string.Empty;
-
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
 
