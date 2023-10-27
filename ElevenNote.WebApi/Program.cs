5d18f42251a9e08be7bc9b7bfa8b1e44d53098df
Sam Page <samnpage96@gmail.com>
2023-10-27 13:43:59 -0400

    added TokenService to builder services

--- ElevenNote.WebApi/Program.cs
+++ ElevenNote.WebApi/Program.cs
@@ -1,6 +1,7 @@
 using System.Text;
 using ElevenNote.Data;
 using ElevenNote.Data.Entities;
+using ElevenNote.Services.Token;
 using ElevenNote.Services.User;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
@@ -48,8 +49,11 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     });
 
 builder.Services.AddControllers();
+
 // Add User Service/Interface for Dependancy Injection here
 builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ITokenService, TokenService>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
