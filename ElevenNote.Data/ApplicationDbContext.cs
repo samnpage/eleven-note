5da4cde310541ce58b14d0f8096c15f85557dd79
Sam Page <samnpage96@gmail.com>
2023-10-26 20:52:52 -0400

    DbContext replaced with Identity framework

--- ElevenNote.Data/ApplicationDbContext.cs
+++ ElevenNote.Data/ApplicationDbContext.cs
@@ -1,12 +1,21 @@
 using ElevenNote.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ElevenNote.Data;
 
-public class ApplicationDbContext : DbContext
+public class ApplicationDbContext : IdentityDbContext<UserEntity, IdentityRole<int>, int>
 {
     // Constructor that passes in options that apply to dbcontext that pulls from program.cs. (passing it the connection string.)
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
-    public DbSet<UserEntity> Users { get; set; } = null!;
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<UserEntity>().ToTable("Users");
+    }
+
+    // public DbSet<UserEntity> Users { get; set; } = null!;
 }
