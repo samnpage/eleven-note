11ce37db8d5146a0e722bfe588026b7e860fd5fe
Sam Page <samnpage96@gmail.com>
2023-10-26 21:58:08 -0400

    Integrated Identity framework, updated UserService constructor, and RegisterUserAsync Method.

--- ElevenNote.Services/User/UserService.cs
+++ ElevenNote.Services/User/UserService.cs
@@ -1,56 +1,77 @@
 using ElevenNote.Data;
 using ElevenNote.Data.Entities;
 using ElevenNote.Models.User;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace ElevenNote.Services.User;
 
 public class UserService : IUserService
 {
-    // Field
+    // Fields
     private readonly ApplicationDbContext _context;
+    private readonly UserManager<UserEntity> _userManager;
+    private readonly SignInManager<UserEntity> _signInManager;
 
     // Constructor that applies ApplicationDbContext's value to a readonly field above^.
-    public UserService(ApplicationDbContext context)
+    public UserService(ApplicationDbContext context,
+                        UserManager<UserEntity> userManager,
+                        SignInManager<UserEntity> signInManager)
+
     {
         _context = context;
+        _userManager = userManager;
+        _signInManager = signInManager;
     }
+
+    
+
+    // public UserService(ApplicationDbContext context)
+    // {
+    //     _context = context;
+    // }
     public async Task<bool> RegisterUserAsync(UserRegister model)
     {
         //  checks the returned value from both methods. If either return anything but null, we'll know it's invalid data.
-        if (await GetUserByEmailAsync(model.Email) != null || await GetUserByUsernameAsync(model.UserName) != null)
+        if (await CheckEmailAvailability(model.Email) == false || await CheckUserNameAvailability(model.UserName) == false)
             return false;
 
         // Calls our UserEntity entity and applys each property value collected to its respective property.
         UserEntity entity = new()
         {
             Email = model.Email,
-            Username = model.UserName,
-            Password = model.Password,
+            UserName = model.UserName,
             DateCreated = DateTime.Now
         };
 
+        IdentityResult registerResult = await _userManager.CreateAsync(entity, model.Password);
+
+        return registerResult.Succeeded;
+
         //Checks if username exists in the database or not.      
         // Adds our new entity object to _context.Users DbSet. This will add the entity to the Users table.
-        _context.Users.Add(entity);
+        // _context.Users.Add(entity);
         // Returns number of rows changed in the db and stores it into a variable.
-        int numberOfChanges = await _context.SaveChangesAsync();
+        // int numberOfChanges = await _context.SaveChangesAsync();
 
         // returns a boolean value of true because we are expecting at least a single change.
-        return numberOfChanges == 1;
+        // return numberOfChanges == 1;
 
     }
 
     // Helper Methods
     // Checks whether the user's email is unique
-    private async Task<UserEntity?> GetUserByEmailAsync(string email)
+    private async Task<bool> CheckEmailAvailability(string email)
     {
-        return await _context.Users.FirstOrDefaultAsync(user => user.Email.ToLower() == email.ToLower());
+        UserEntity? existingUser = await _userManager.FindByEmailAsync(email);
+        return existingUser is null;
     }
 
     // Checks whether the user's username is unique
-    private async Task<UserEntity?> GetUserByUsernameAsync(string username)
+    private async Task<bool> CheckUserNameAvailability(string userName)
     {
-        return await _context.Users.FirstOrDefaultAsync(user => user.Username.ToLower() == username.ToLower());
+        UserEntity? existingUser = await _userManager.FindByNameAsync(userName);
+        return existingUser is null;
     }
 }
