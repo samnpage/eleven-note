30ee000bd861f52b59e8f86ddbd93d2896009302
Sam Page <samnpage96@gmail.com>
2023-10-27 12:20:11 -0400

    Authentication middleware added

--- ElevenNote.WebApi/Program.cs
+++ ElevenNote.WebApi/Program.cs
@@ -45,6 +45,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// Adds AuthenticationMiddleware to the IApplicationBuilder
+// This enables authentication capabilities
+app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllers();
