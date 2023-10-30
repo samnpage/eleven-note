2343181443a57eef05cf21f6fd285d917519528f
Sam Page <samnpage96@gmail.com>
2023-10-30 16:38:51 -0400

    DeleteNote DELETE endpoint added

--- ElevenNote.WebApi/Controllers/NoteController.cs
+++ ElevenNote.WebApi/Controllers/NoteController.cs
@@ -66,4 +66,13 @@ public class NoteController : ControllerBase
             ? Ok("Note updated successfully.")
             : BadRequest("Note could not be updated.");
     }
+
+    // DELETE api/Note/5
+    [HttpDelete("noteId:int")]
+    public async Task<IActionResult> DeleteNote([FromRoute] int noteId)
+    {
+        return await _noteService.DeleteNoteAsync(noteId)
+            ? Ok($"Note {noteId} was deleted successfully.")
+            : BadRequest($"Note {noteId} could not be deleted");
+    }
 }
