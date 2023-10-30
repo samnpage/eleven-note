6dabad55bc17b01fa8929b0cc15d11644a51b126
Sam Page <samnpage96@gmail.com>
2023-10-30 15:33:19 -0400

    injected NoteServices

--- ElevenNote.WebApi/Controllers/NoteController.cs
+++ ElevenNote.WebApi/Controllers/NoteController.cs
@@ -1,3 +1,4 @@
+using ElevenNote.Services.Note;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ElevenNote.WebApi.Controllers;
@@ -6,5 +7,10 @@ namespace ElevenNote.WebApi.Controllers;
 [ApiController]
 public class NoteController : ControllerBase
 {
-    
+    private readonly INoteService _noteService;
+
+    public NoteController(INoteService noteService)
+    {
+        _noteService = noteService;
+    }
 }
