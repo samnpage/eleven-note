67c67322c10fdaf1de39f7f6dfdff461d0466c1d
Sam Page <samnpage96@gmail.com>
2023-10-30 15:35:12 -0400

    added Authorization to entire controller

--- ElevenNote.WebApi/Controllers/NoteController.cs
+++ ElevenNote.WebApi/Controllers/NoteController.cs
@@ -1,8 +1,10 @@
 using ElevenNote.Services.Note;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ElevenNote.WebApi.Controllers;
 
+[Authorize]
 [Route("api/[controller]")]
 [ApiController]
 public class NoteController : ControllerBase
