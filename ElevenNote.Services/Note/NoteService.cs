c26c378e7d09e049e8d1384cb22f37f6f340e8be
Sam Page <samnpage96@gmail.com>
2023-10-30 15:22:47 -0400

    NoteService constructor added

--- ElevenNote.Services/Note/NoteService.cs
+++ ElevenNote.Services/Note/NoteService.cs
@@ -1,6 +1,26 @@
+using ElevenNote.Data;
+using ElevenNote.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+
 namespace ElevenNote.Services.Note;
 
 public class NoteService : INoteService
 {
+    private readonly ApplicationDbContext _dbContext;
+    private readonly int _userId;
 
+    public NoteService(UserManager<UserEntity> userManager,
+                        SignInManager<UserEntity> signInManager,
+                        ApplicationDbContext dbContext)
+    {
+        var currentUser = signInManager.Context.User;
+        var userIdClaim = userManager.GetUserId(currentUser);
+        var hasValidId = int.TryParse(userIdClaim, out _userId);
+
+        if (!hasValidId)
+            throw new Exception("Attempted to build NoteService without Id Claim.");
+
+        _dbContext = dbContext;
+    }
 }
