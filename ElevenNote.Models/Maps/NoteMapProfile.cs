8a156cd82e20507aa1d5c1e73b9561ecf4128829
Sam Page <samnpage96@gmail.com>
2023-10-31 15:11:10 -0400

    initial commit

---
+++ ElevenNote.Models/Maps/NoteMapProfile.cs
@@ -0,0 +1,21 @@
+using AutoMapper;
+using ElevenNote.Data.Entities;
+using ElevenNote.Models.Note;
+
+namespace ElevenNote.Models.Maps;
+
+public class NoteMapProfile : Profile
+{
+    public NoteMapProfile()
+    {
+        // Map from NoteEntity to NoteDetail/NoteListItem
+        CreateMap<NoteEntity, NoteDetail>();
+        CreateMap<NoteEntity, NoteListItem>();
+
+        // Map from NoteCreate to NoteEntity, and establish current DateTimeOffset
+        CreateMap<NoteCreate, NoteEntity>()
+            .ForMember(note => note.CreatedUtc,
+                opt => opt.MapFrom(src => DateTimeOffset.Now)
+            );
+    }
+}
